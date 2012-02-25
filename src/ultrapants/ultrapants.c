cc49ee6e47bc8127afe08b63c82fb7df66d96251
Denis Defreyne <denis.defreyne@stoneship.org>
2012-02-25 17:45:34 +0100

    added support for quotes

--- src/ultrapants/ultrapants.c
+++ src/ultrapants/ultrapants.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdlib.h>
 
 enum _up_state
@@ -27,10 +28,48 @@ static size_t _up_write_mdash(char *out)
 	return 3;
 }
 
-void ultrapants(char *in, char *out)
+static size_t _up_write_single_quote_start(char *out)
+{
+	*out++ = 0xE2;
+	*out++ = 0x80;
+	*out++ = 0x98;
+
+	return 3;
+}
+
+static size_t _up_write_single_quote_end(char *out)
+{
+	*out++ = 0xE2;
+	*out++ = 0x80;
+	*out++ = 0x99;
+
+	return 3;
+}
+
+static size_t _up_write_double_quote_start(char *out)
+{
+	*out++ = 0xE2;
+	*out++ = 0x80;
+	*out++ = 0x9C;
+
+	return 3;
+}
+
+static size_t _up_write_double_quote_end(char *out)
+{
+	*out++ = 0xE2;
+	*out++ = 0x80;
+	*out++ = 0x9D;
+
+	return 3;
+}
+
+void ultrapants(char *a_in, char *a_out)
 {
 	enum _up_state state = _up_state_start;
 
+	char *in  = a_in;
+	char *out = a_out;
 	for (; *in; ++in)
 	{
 		switch (state)
@@ -42,16 +81,30 @@ void ultrapants(char *in, char *out)
 			case _up_state_dashdash: goto UP_STATE_DASHDASH; break;
 		}
 
-		UP_STATE_START:
+	UP_STATE_START:
 		if (*in == '.')
 			state = _up_state_dot;
 		else if (*in == '-')
 			state = _up_state_dash;
+		else if (*in == '\'')
+		{
+			if (in > a_in && isspace(*(in-1)))
+				out += _up_write_single_quote_start(out);
+			else
+				out += _up_write_single_quote_end(out);
+		}
+		else if (*in == '"')
+		{
+			if (in > a_in && isspace(*(in-1)))
+				out += _up_write_double_quote_start(out);
+			else
+				out += _up_write_double_quote_end(out);
+		}
 		else
 			*out++ = *in;
 		continue;
 
-		UP_STATE_DOT:
+	UP_STATE_DOT:
 		if (*in == '.')
 			state = _up_state_dotdot;
 		else
@@ -62,7 +115,7 @@ void ultrapants(char *in, char *out)
 		}
 		continue;
 
-		UP_STATE_DOTDOT:
+	UP_STATE_DOTDOT:
 		if (*in == '.')
 		{
 			out += _up_write_ellipsis(out);
@@ -77,7 +130,7 @@ void ultrapants(char *in, char *out)
 		}
 		continue;
 
-		UP_STATE_DASH:
+	UP_STATE_DASH:
 		if (*in == '-')
 			state = _up_state_dashdash;
 		else
@@ -88,7 +141,7 @@ void ultrapants(char *in, char *out)
 		}
 		continue;
 		
-		UP_STATE_DASHDASH:
+	UP_STATE_DASHDASH:
 		if (*in == '-')
 		{
 			out += _up_write_mdash(out);
