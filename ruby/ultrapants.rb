f50a679d182be7f9b6170a7d2f5cf6c087319807
Denis Defreyne <denis.defreyne@stoneship.org>
2012-04-14 11:12:03 +0200

    fixed memleak in ruby bindings

--- ruby/ultrapants.rb
+++ ruby/ultrapants.rb
@@ -6,12 +6,17 @@ require 'ffi'
 module Ultrapants
   extend FFI::Library
 
+  ffi_lib FFI::Library::LIBC
   ffi_lib 'ultrapants'
 
-  attach_function :_ultrapants, :ultrapants, [ :string, :int ], :string
+  attach_function :_ultrapants, :ultrapants, [ :string, :int ], :pointer
+  attach_function :_free,       :free,       [ :pointer      ], :void
 
   def self.process(t)
-    _ultrapants(t, t.size)
+    ptr = _ultrapants(t, t.size)
+    str = ptr.read_string_to_null
+    _free(ptr)
+    str
   end
 
 end
