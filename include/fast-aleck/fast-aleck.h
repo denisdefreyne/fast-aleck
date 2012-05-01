c72ca6803d6221b1f10c2db73f487dfb2e2eda12
Denis Defreyne <denis.defreyne@stoneship.org>
2012-05-01 10:17:02 +0200

    documented fast_aleck c function

--- include/fast-aleck/fast-aleck.h
+++ include/fast-aleck/fast-aleck.h
@@ -1,6 +1,8 @@
 #ifndef __FAST_ALECK_FAST_ALECK_H__
 #define __FAST_ALECK_FAST_ALECK_H__
 
+#include <string.h>
+
 typedef char fa_bool;
 
 typedef struct _fast_aleck_config {
@@ -8,6 +10,13 @@ typedef struct _fast_aleck_config {
 	fa_bool wrap_quotes;
 } fast_aleck_config;
 
+// params:
+//   char *in - the unenhanced input string
+//   size_t in_size - the size, in bytes, of the unenhanced input string
+//   size_t *ao_len - a pointer to where the size of the returned string should be written (can be NULL)
+//
+// return:
+//   the enhanced string, allocated on the heap
 char *fast_aleck(fast_aleck_config config, char *in, size_t in_size, size_t *ao_len);
 
 #endif
