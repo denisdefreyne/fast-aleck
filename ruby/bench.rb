ba8df1fc58e3ca980ebce2956ec87c9ee7a284cd
Denis Defreyne <denis.defreyne@stoneship.org>
2012-05-01 09:57:22 +0200

    made benchmarks faster and more accurate

--- ruby/bench.rb
+++ ruby/bench.rb
@@ -7,18 +7,18 @@ require 'typogruby'
 alice = File.read('../input/alice.txt')
 
 Benchmark.bmbm do |r|
-  r.report('alice-rubypants') do 
-    100.times do
+  r.report('alice-rubypants (x10)') do 
+    500.times do
       RubyPants.new(alice).to_html
     end
   end
-  r.report('alice-typogruby') do 
-    100.times do
+  r.report('alice-typogruby (x100)') do 
+    50.times do
       Typogruby.improve(alice)
     end
   end
   r.report('alice-fast-aleck') do 
-    100.times do
+    5000.times do
       config = FastAleck::Config.new
       config[:wrap_amps] = 1
       FastAleck.process(config, alice)
